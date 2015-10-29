2e9595ebefa92b8f0bdbc57ce4b0cacb824e124e
Lars Pötter <justanotherone@gmx.net>
2015-10-29 21:31:23 +0100

    added Test for G-Code decoder.

---
+++ tests/src/de/nomagic/printerController/core/TestGCodeDecoder.java
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+package de.nomagic.printerController.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author lars
+ *
+ */
+public class TestGCodeDecoder
+{
+
+	/**
+	 * Test method for {@link de.nomagic.printerController.core.GCodeDecoder#GCodeDecoder(de.nomagic.printerController.core.Executor, de.nomagic.printerController.Cfg)}.
+	 */
+	@Test
+	public void testGCodeDecoder()
+	{
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.nomagic.printerController.core.GCodeDecoder#sendLine(java.lang.String, de.nomagic.printerController.GCodeResultStream)}.
+	 */
+	@Test
+	public void testSendLine()
+	{
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.nomagic.printerController.core.GCodeDecoder#getLastErrorReason()}.
+	 */
+	@Test
+	public void testGetLastErrorReason()
+	{
+		fail("Not yet implemented");
+	}
+
+}
