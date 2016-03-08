c70412163d7b8598d1643deb741e3cfa61b17d80
Lars Pötter <justanotherone@gmx.net>
2016-03-08 22:13:42 +0100

    switched Wizard from Swing to JavaFX.

---
+++ src/de/nomagic/printerController/createConfigFx/Main.java
@@ -0,0 +1,80 @@
+package de.nomagic.printerController.createConfigFx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application
+{
+    private BorderPane SessionDefinitionScene;
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		System.out.println("Starting Configuration Creator in version : " + getCommitID());
+
+        // Create GUI
+        primaryStage.setTitle("Pacemaker Configuration creator");
+        MainWindowController ctrl = new MainWindowController(primaryStage);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("view/SessionDefinitionScene.fxml"));
+        loader.setController(ctrl);
+        try
+        {
+            SessionDefinitionScene = (BorderPane) loader.load();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // Show the scene containing the root layout.
+        Scene scene = new Scene(SessionDefinitionScene);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+
+    private String getCommitID()
+    {
+        try
+        {
+            final InputStream s = getClass().getResourceAsStream("/commit-id");
+            final BufferedReader in = new BufferedReader(new InputStreamReader(s));
+            final String commitId = in.readLine();
+            final String changes = in.readLine();
+            if(null != changes)
+            {
+                if(0 < changes.length())
+                {
+                    return commitId + "-(" + changes + ")";
+                }
+                else
+                {
+                    return commitId;
+                }
+            }
+            else
+            {
+                return commitId;
+            }
+        }
+        catch( Exception e )
+        {
+            return "could not read version: " + e.toString();
+        }
+    }
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
---
+++ src/de/nomagic/printerController/createConfigFx/MainWindowController.java
@@ -0,0 +1,13 @@
+package de.nomagic.printerController.createConfigFx;
+
+import javafx.stage.Stage;
+
+public class MainWindowController
+{
+
+	public MainWindowController(Stage primaryStage)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
